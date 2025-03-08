ffc29658dcd5c90147f94ff83ab233bd8ec5f805
CatMusso <egor.san.2005@gmail.com>
2025-03-08 16:13:44 +0700

    done js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -2,39 +2,36 @@
     "use strict";
 
     const root = document.documentElement;
+    const navToogle = document.querySelector("#js-navToggle");
 
-    const navToggle = document.querySelector("#js-navToggle");
-    navToggle.addEventListener("click", function () {
+    navToogle.addEventListener("click", function () {
         root.classList.toggle("show-nav");
     });
 
     const eventPP = document.querySelector("#js-eventPP");
-    const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
-
-    if (eventPP && eventOpenBtn) {
+    if (eventPP) {
+        const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
         const closeEventPP = function (event) {
             function close() {
                 document.removeEventListener("keyup", closeEventPP);
                 eventPP.removeEventListener("click", closeEventPP);
-
                 root.classList.remove("show-event-popup");
             }
-
             switch (event.type) {
-                case "keyup": {
+                case "keyup":
                     if (event.key === "Escape" || event.keyCode === 27) close();
                     break;
-                }
-                case "click": {
-                    if (event.target === this || event.target.classList.contains("js-ppCloseBtn")) close();
+                case "click":
+                    if (
+                        event.target === this ||
+                        event.target.classList.contains("js-ppCloseBtn")
+                    )
+                        close();
                     break;
-                }
             }
-        }
-
+        };
         eventOpenBtn.addEventListener("click", function () {
             root.classList.add("show-event-popup");
-
             document.addEventListener("keyup", closeEventPP);
             eventPP.addEventListener("click", closeEventPP);
         });
